6fd44001cd649ea4f9ccdf89b6028e1348e72fa7
yjglab <yjgdesign@gmail.com>
2022-03-30 02:08:18 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import Button from "./Button";
 import styles from "./App.module.css";
-function App() {
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const [cnt, setCnt] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setCnt((p) => p + 1);
+  const onChange = (e) => {
+    setKeyword(e.target.value);
+  };
+  console.log("rendered All time");
+  useEffect(() => console.log("rendered one time"), []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) console.log("keyboard on");
+  }, [keyword]);
   return (
     <div>
+      <input
+        onChange={onChange}
+        value={keyword}
+        type="text"
+        placeholder="Search..."
+      />
       <h1 className={styles.title}>home</h1>
-      <Button text={"버튼"} />
+      <span>{cnt}</span>
+      <button onClick={onClick}>click</button>
     </div>
   );
-}
+};
 
 export default App;
