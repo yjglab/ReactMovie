b99682182dccc325a940db8ff28277fd1a0de0ea
yjglab <yjgdesign@gmail.com>
2022-03-31 21:29:16 +0900

    coin tracking

--- src/CoinTracker.js
+++ src/CoinTracker.js
@@ -1,9 +1,32 @@
-import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
 const CoinTracker = () => {
-  const [loading, setLoading] = useState();
-  return <div>dd</div>;
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  console.log(coins);
+  return (
+    <div>
+      {loading ? (
+        <strong>로딩중</strong>
+      ) : (
+        <select>
+          {coins.map((c) => (
+            <option key={c.id}>
+              {c.name} ({c.symbol}): ${c.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+    </div>
+  );
 };
 
 export default CoinTracker;
